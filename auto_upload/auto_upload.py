97388a267ed716941ec8913c2c91b2397e766464
KurisuNya <cuizhu1216@gmail.com>
2023-02-05 19:05:42 +0800

    style: 代码格式化

--- auto_upload/auto_upload.py
+++ auto_upload/auto_upload.py
@@ -9,18 +9,18 @@ def album_upload(album_config):
     """
     Usage:
         根据输入的album_config内的相册参数进行图片上传
-    
+
     Args:
         album_config (_dict_):{
-            "name": _str_ ,  # 相册名称 
+            "name": _str_ ,  # 相册名称
             "path": _str_ ,  # 相册文件夹路径
-            "permission": _str_ ,  # 图片权限-"public"或"private" 
+            "permission": _str_ ,  # 图片权限-"public"或"private"
             "delete_image": _bool_ ,  # 是否在上传后删除图片
             "time_rename": _bool_  # 是否使用时间戳重命名文件
         }
     """
     # 相册参数设置
-    album_id = picture_bed.get_album_id(album_config["name"])  
+    album_id = picture_bed.get_album_id(album_config["name"])
     album_path = album_config["path"]
     album_permission = album_config["permission"]
     album_delete_image = album_config["delete_image"]
@@ -29,33 +29,41 @@ def album_upload(album_config):
     if album_id["get_status"] is True:
         album_id = album_id["album_id"]
         # 遍历相册路径
-        for root, dirs, files in os.walk(album_path, topdown=False):  
+        for root, dirs, files in os.walk(album_path, topdown=False):
             for name in files:
-                file_path = os.path.join(root, name)  
+                file_path = os.path.join(root, name)
                 # 忽略目录下unsupported_files与uploaded_images文件夹
-                if ("unsupported_files" not in file_path) & ("uploaded_images" not in file_path):  
+                if ("unsupported_files" not in file_path) & (
+                    "uploaded_images" not in file_path
+                ):
                     # 检测文件格式是否支持
-                    if check_image(name) is True:  
+                    if check_image(name) is True:
                         # 设置项检测：时间戳重命名
-                        if album_time_rename is True:  
+                        if album_time_rename is True:
                             file_path = time_rename(file_path)
                         # 图片上传
-                        response = picture_bed.image_upload(file_path, album_permission, album_id)
+                        response = picture_bed.image_upload(
+                            file_path, album_permission, album_id
+                        )
                         # 设置项检测：删除上传图片
-                        if response & album_delete_image is True:  
+                        if response & album_delete_image is True:
                             os.remove(file_path)
-                        elif response & album_delete_image is False:  
+                        elif response & album_delete_image is False:
                             uploaded_path = os.path.join(album_path, "uploaded_images")
                             if "uploaded_images" not in os.listdir(album_path):
                                 os.mkdir(uploaded_path)
-                            if os.path.exists(os.path.join(uploaded_path,os.path.basename(file_path))):
+                            if os.path.exists(
+                                os.path.join(uploaded_path, os.path.basename(file_path))
+                            ):
                                 file_path = name_add_time(file_path)
                             shutil.move(file_path, uploaded_path)
-                    else:  
+                    else:
                         move_path = os.path.join(album_path, "unsupported_files")
                         if "unsupported_files" not in os.listdir(album_path):
                             os.mkdir(move_path)
-                        if os.path.exists(os.path.join(move_path,os.path.basename(file_path))):
+                        if os.path.exists(
+                            os.path.join(move_path, os.path.basename(file_path))
+                        ):
                             file_path = name_add_time(file_path)
                         shutil.move(file_path, move_path)
 
@@ -72,21 +80,30 @@ def check_image(file_name):
         _bool_: 文件格式是否支持
     """
     file_suffix = os.path.splitext(file_name)[1].lower()
-    allow_suffix = {".jpeg", ".jpg", ".png", ".gif",
-                    ".tif", ".bmp", ".ico", ".psd", ".webp"}
+    allow_suffix = {
+        ".jpeg",
+        ".jpg",
+        ".png",
+        ".gif",
+        ".tif",
+        ".bmp",
+        ".ico",
+        ".psd",
+        ".webp",
+    }
     if file_suffix in allow_suffix:
         return True
     else:
         return False
 
 
-def get_current_time():  
+def get_current_time():
     """
     Usage:
         返回格式为"Y-M-D-H-M-S-MS"的时间戳
 
     Returns:
-        _str_: 当前时间时间戳 
+        _str_: 当前时间时间戳
     """
     current_time = time.time()
     local_time = time.localtime(current_time)
@@ -96,22 +113,22 @@ def get_current_time():
     return time_stamp
 
 
-def time_rename(file_path):  
+def time_rename(file_path):
     """
     Usage:
         使用时间戳重命名文件
 
     Args:
-        file_path (_str_): 文件路径 
+        file_path (_str_): 文件路径
 
     Returns:
-        _str_ : 重命名后的文件路径 
+        _str_ : 重命名后的文件路径
     """
     time_stamp = get_current_time()
     file_spilt = os.path.splitext(file_path)
     file_suffix = file_spilt[1].lower()
     file_root = os.path.dirname(file_path)
-    rename_path = os.path.join(file_root, time_stamp+file_suffix)
+    rename_path = os.path.join(file_root, time_stamp + file_suffix)
     os.rename(file_path, rename_path)
     return rename_path
 
@@ -122,17 +139,17 @@ def name_add_time(file_path):
         在文件名称结尾添加时间戳
 
     Args:
-        file_path (_str_): 文件路径 
+        file_path (_str_): 文件路径
 
     Returns:
-        _str_ : 重命名后的文件路径 
+        _str_ : 重命名后的文件路径
     """
     time_stamp = get_current_time()
     file_spilt = os.path.splitext(os.path.basename(file_path))
     file_name = file_spilt[0]
     file_suffix = file_spilt[1].lower()
     file_root = os.path.dirname(file_path)
-    rename_path = os.path.join(file_root,file_name+"_"+time_stamp+file_suffix)
+    rename_path = os.path.join(file_root, file_name + "_" + time_stamp + file_suffix)
     os.rename(file_path, rename_path)
     return rename_path
 
--- auto_upload/picture_bed.py
+++ auto_upload/picture_bed.py
@@ -6,48 +6,51 @@ import json
 config = json.load(open("auto_upload.config"))
 server = config["server"]
 token = config["token"]
-# 判断是否开启忽略SSL 
-if config["disable_ssl"] is True:  
+# 判断是否开启忽略SSL
+if config["disable_ssl"] is True:
     requests.urllib3.disable_warnings()
     server_verify = False
 else:
     server_verify = True
 
 
-def image_upload(image_path,permission,album_id):  
+def image_upload(image_path, permission, album_id):
     """
     Usage:
         根据传输参数调用api上传图片
 
     Args:
         image_path (_str_): 图片路径
-        permission (_str_): 图片权限-"public"或"private" 
+        permission (_str_): 图片权限-"public"或"private"
         album_id (_int_): 相册id
 
     Returns:
         _bool_: 上传是否成功
     """
     # 格式化permission参数为api所定义的值
-    if permission == "public":  
+    if permission == "public":
         format_permission = 1
     else:
         format_permission = 0
     # 设置POST参数
     upload_header = {
-        "Accept":"application/json",
-        "Authorization":token,
+        "Accept": "application/json",
+        "Authorization": token,
     }
     upload_file = {
-            "file":open(image_path,"rb"),
+        "file": open(image_path, "rb"),
     }
-    upload_data = {
-            "permission":format_permission,
-            "album_id":album_id
-    }
-    # POST上传图片   
-    response = requests.post(url=server+"/api/v1/upload",files=upload_file,data=upload_data,headers=upload_header,verify=server_verify)
+    upload_data = {"permission": format_permission, "album_id": album_id}
+    # POST上传图片
+    response = requests.post(
+        url=server + "/api/v1/upload",
+        files=upload_file,
+        data=upload_data,
+        headers=upload_header,
+        verify=server_verify,
+    )
     # 判断是否上传成功
-    if "status" in response.json():  
+    if "status" in response.json():
         if response.json()["status"] is True:
             return True
         else:
@@ -56,7 +59,7 @@ def image_upload(image_path,permission,album_id):
         return False
 
 
-def get_album_id(album_name):  
+def get_album_id(album_name):
     """
     Usage:
         获取相册id
@@ -72,22 +75,25 @@ def get_album_id(album_name):
     """
     # 设置GET参数
     get_header = {
-        "Accept":"application/json",
-        "Authorization":token,
+        "Accept": "application/json",
+        "Authorization": token,
     }
-    get_params = {
-        "q":album_name
-    }
+    get_params = {"q": album_name}
     # GET获取相册id
-    response = requests.get(url=server+"/api/v1/albums",params=get_params,headers=get_header,verify=server_verify)
+    response = requests.get(
+        url=server + "/api/v1/albums",
+        params=get_params,
+        headers=get_header,
+        verify=server_verify,
+    )
     # 判断是否找到相册id
     if "status" in response.json():
         if response.json()["status"] is True:
-            for album in response.json()["data"]["data"]:  
+            for album in response.json()["data"]["data"]:
                 if album["name"] == album_name:
-                    return {"get_status":True,"album_id":album["id"]}
-            return {"get_status":True,"album_id":None}
+                    return {"get_status": True, "album_id": album["id"]}
+            return {"get_status": True, "album_id": None}
         else:
-            return {"get_status":False,"album_id":None}
+            return {"get_status": False, "album_id": None}
     else:
-        return {"get_status":False,"album_id":None}
+        return {"get_status": False, "album_id": None}
